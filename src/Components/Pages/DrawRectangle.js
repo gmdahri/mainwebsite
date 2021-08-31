c80a2a11bb9c3228e7799963f924338d820387ba
gmdahri <gm24139@gmail.com>
2021-08-31 17:15:31 +0500

    updated

--- src/Components/Pages/DrawRectangle.js
+++ src/Components/Pages/DrawRectangle.js
@@ -8,25 +8,26 @@ import {
 } from 'draw-shape-reactjs';
 import img from '../assets/frame218.jpg';
 
-export default function DrawRectangle(x,y,w,h) {
-    console.log(x,y,w,h)
-    
+export default function DrawRectangle(state) {
+    // console.log(state.state , 'in rectangle ')
+    var x=[1];
     return (
         <div>
 
-{/* <img src={img} /> */}
+<img src={img} /> 
              {/* <Line
                     position='fixed'
                     from={[1469.4885341291, 1029.933670802]}
                     to={[1050.7102464445 , 173.8927916782 ]}
                     color='#1DBFE7'
                 /> */}
-           <Rectangle
-                corner={x,y}
-                width={w - x}
-                height={h - y }
+                {state && state.state && console.log(state.state)}
+        {  state && <Rectangle
+                corner={[state.state.x, state.state.y]}
+                width={state.state.w - state.state.x}
+                height={state.state.h - state.state.y}
                 color='#FF0266'
-            />
+            />}
         </div>
     )
 }
--- src/Components/Pages/ReadCSV.js
+++ src/Components/Pages/ReadCSV.js
@@ -9,53 +9,44 @@ import {
     Rectangle
 } from 'draw-shape-reactjs';
 export default function ReadCSV() {
-    const [fileData, setfileData] = useState([]);
-    const [state, setstate] = useState([]);
-    let x=[];
-    let y=[];
-    let w=[];
-    let h=[];
+    const [state, setstate] = useState({});
+  
     const itterateData = (data) => {
         for (let i = 0; i < data.length; i++)
             for (let j = 0; j < data[i].length; j++) {
-                // console.log(data[i][1]);
                 if (data[i][1] == '218') {
                    
-                    x=data[i][2];
-                    y=data[i][3];
-                    w=data[i][4];
-                    h=data[i][5];
-                    
+                  setstate([...state, {
+                  x: Number(data[i][2]), 
+                  y: Number(data[i][3]),
+                  w: Number(data[i][4]), 
+                  h: Number(data[i][5]) }]);
+
+
+                  
                     break;
                 }
                 else {
                     console.log('no')
                 }
-            }
+          }
 
-        // for(let i=0;i<checkFrame.length;i++){
-        //     if(checkFrame[i]===218)
-        //     setframe(checkFrame[i]);
-        // }
-        
-        // console.dir(state)
+
+          console.log(state)
+    //    console.log(state,'in function')
     }
     return (
         <div>
             <CSVReader onFileLoaded={(data, fileInfo) => itterateData(data)} />
-            {/* <input type='file' multiple /> */} 
-             <DrawRectangle x={x} y={y} w={w} h={h} />
+           
            {
-    console.log(x,y,w,h)
+             
+            //   console.log(state ," state")
            }
-            <Rectangle
-                corner={x,y}
-                width={w - x}
-                height={h - y }
-                color='#FF0266'
-            />
-            
+             {state && (<DrawRectangle  state={state}/>)} 
+             
 
+           
         </div>
     )
 }
