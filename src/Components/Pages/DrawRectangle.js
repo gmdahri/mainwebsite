c3574a1f427b79f5c2c2a7c45c410ef6610ab988
gmdahri <gm24139@gmail.com>
2021-09-01 16:49:58 +0500

    updated

--- src/Components/Pages/DrawRectangle.js
+++ src/Components/Pages/DrawRectangle.js
@@ -6,28 +6,29 @@ import {
     Circle,
     Rectangle
 } from 'draw-shape-reactjs';
-import img from '../assets/frame218.jpg';
 
 export default function DrawRectangle(state) {
     // console.log(state.state , 'in rectangle ')
+    console.log(state.state,'pxa');
     var x=[1];
     return (
         <div>
 
-<img src={img} /> 
-             {/* <Line
-                    position='fixed'
-                    from={[1469.4885341291, 1029.933670802]}
-                    to={[1050.7102464445 , 173.8927916782 ]}
-                    color='#1DBFE7'
-                /> */}
-                {state && state.state && console.log(state.state)}
+        {/* {pxa && pxb &&    <Line
+                position='fixed'
+                from={[pxa, pya]}
+                to={[pxb , pyb ]}
+                color='#1DBFE7'
+            />} */}
+
+{/* {state && state.state && console.log(state.state[0].x)} */}
+
         {  state && <Rectangle
                 corner={[state.state.x, state.state.y]}
                 width={state.state.w - state.state.x}
                 height={state.state.h - state.state.y}
                 color='#FF0266'
-            />}
+            />} 
         </div>
     )
 }
--- src/Components/Pages/ReadCSV.js
+++ src/Components/Pages/ReadCSV.js
@@ -8,47 +8,52 @@ import {
     Circle,
     Rectangle
 } from 'draw-shape-reactjs';
+import img from '../assets/frame218.jpg';
+
 export default function ReadCSV() {
     const [state, setstate] = useState([]);
+    const [value,setValue]=useState([]);
     useEffect(() => {
-        console.log(state);
+        // console.log(state, 'in useEffect');
     }, [state])
-      
     const itterateData = (data) => {
-        for (let i = 0; i < data.length; i++)
-            for (let j = 0; j < data[i].length; j++) {
-                if (data[i][1] == '218') {
-                   
-                  setstate([...state,{ 
+        for (let i = 0; i < data.length; i++) {
+         if (data[i][1] == '218') {
+                setstate([...state,{ 
                   x: Number(data[i][2]), 
                   y: Number(data[i][3]),
                   w: Number(data[i][4]), 
                   h: Number(data[i][5])}]);
-
-
-                  
-                    break;
-                }
-                else {
-                    console.log('no')
+                     break;
                 }
           }
+        // for(var i=0;i<data.length;i++){
+        //     for(var j=0;j<data[i].length;j++){
+        //         if(data[i][6]!='xmid')
+        //         setstate([...state,
+        //         {
+        //             x:data[i][6],
+        //             y:data[i][7]
+        //         }]);
+        //     }
+        // }
 
-          console.log(state)
-    //    console.log(state,'in function')
     }
     return (
         <div>
+
             <CSVReader onFileLoaded={(data, fileInfo) => itterateData(data)} />
            
            {
              
-            //   console.log(state ," state")
+            //   console.log(state ," state")  
            }
 
              {state && state.map((e)=>(<DrawRectangle  state={e}/>))} 
-             
-
+             {/* {value && value.map((e,i)=>(<DrawRectangle pOne={e[i]} pTwo={e[i+1]} />))} */}
+         {/* {state && <DrawRectangle state={state} /> } */}
+             <img src={img} /> 
+                
            
         </div>
     )
