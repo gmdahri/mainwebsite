0a22c0dd81d4028b9d9003a80fa67551179773ac
gmdahri <gmdahri.cs17@iba-suk.edu.pk>
2021-09-02 10:08:28 +0500

    'update..'

--- src/Components/Pages/DrawRectangle.js
+++ src/Components/Pages/DrawRectangle.js
@@ -1,46 +1,44 @@
-import React,{useEffect} from 'react'
+import React, { useEffect } from "react";
 import {
-    Line,
-    SteppedLine,
-    PolyLine,
-    Circle,
-    Rectangle
-} from 'draw-shape-reactjs';
+  Line,
+  SteppedLine,
+  PolyLine,
+  Circle,
+  Rectangle,
+} from "draw-shape-reactjs";
 
-export default function DrawRectangle(state,counter) {
-    // console.log(state.state , 'in rectangle ')
-    console.log(state.state,'pxa');
-    var x=[1];
-    return (
-        <div>
+export default function DrawRectangle({ state }) {
+  useEffect(() => {}, []);
 
-<<<<<<< HEAD
-<img src={img} /> 
-             {/* <Line
-                    position='fixed'
-                    from={[1469.4885341291, 1029.933670802]}
-                    to={[1050.7102464445 , 173.8927916782 ]}
-                    color='#1DBFE7'
-                /> */}
-                {state && state.state && console.log(state.state)}
-        {  state ? for(let i=0;i<counter;i++){}:null}
-=======
-        {/* {pxa && pxb &&    <Line
-                position='fixed'
-                from={[pxa, pya]}
-                to={[pxb , pyb ]}
-                color='#1DBFE7'
-            />} */}
+  // console.log(state.state,'pxa');
+  var x = [1];
+  return (
+    <div>
+      {state &&
+        state.map((element, index) => {
+         index<state.length-10 && (
+            <Line
+              position="fixed"
+              from={[element.x, element.y]}
+              to={[state[index + 1].x, state[index + 1].y]}
+              color="#FFFF00"
+            />)
+          
+        })}
+      {/* <Line
+              position="fixed"
+              from={[state[0].x, state[0].y]}
+              to={[state[10].x, state[10].y]}
+              color="#FFFF00"
+            /> */}
+      {/* {state && state.state && console.log(state.state[0].x)} */}
 
-{/* {state && state.state && console.log(state.state[0].x)} */}
-
-        {  state && <Rectangle
+      {/* {  state && <Rectangle
                 corner={[state.state.x, state.state.y]}
                 width={state.state.w - state.state.x}
                 height={state.state.h - state.state.y}
                 color='#FF0266'
-            />} 
->>>>>>> c3574a1f427b79f5c2c2a7c45c410ef6610ab988
-        </div>
-    )
+            />}  */}
+    </div>
+  );
 }
--- src/Components/Pages/ReadCSV.js
+++ src/Components/Pages/ReadCSV.js
@@ -11,58 +11,35 @@ import {
 import img from '../assets/frame218.jpg';
 
 export default function ReadCSV() {
-<<<<<<< HEAD
-    const [state, setstate] = useState({});
-    var [counter,setCounter]=useState(0);
-    var c=0;
-=======
     const [state, setstate] = useState([]);
-    const [value,setValue]=useState([]);
+    const [flag,setFlag]=useState(false);
     useEffect(() => {
         // console.log(state, 'in useEffect');
-    }, [state])
->>>>>>> c3574a1f427b79f5c2c2a7c45c410ef6610ab988
+    }, [state]);
     const itterateData = (data) => {
-        for (let i = 0; i < data.length; i++) {
-         if (data[i][1] == '218') {
-                setstate([...state,{ 
-                  x: Number(data[i][2]), 
-                  y: Number(data[i][3]),
-                  w: Number(data[i][4]), 
-<<<<<<< HEAD
-                  h: Number(data[i][5]), });
-                  
-                  setCounter(counter+1);
+        // for (let i = 0; i < data.length; i++) {
+        //  if (data[i][1] == '218') {
+        //         setstate([...state,{ 
+        //           x: Number(data[i][2]), 
+        //           y: Number(data[i][3]),
+        //           w: Number(data[i][4]), 
+        //           h: Number(data[i][5])}]);
+        //              break;
+        //         }
+        //   }
+      
+        for(var i=0;i<data.length;i++){
+            
+                if(data[i][6]!='xmid')
+                setstate((pre)=>[...pre,
+                {
+                    x:data[i][6],
+                    y:data[i][7]
+                }]);
+            
+        }
+        setFlag(true);
 
-
-                  
-                    break;
-                }
-                else {
-                    console.log('no')
-=======
-                  h: Number(data[i][5])}]);
-                     break;
->>>>>>> c3574a1f427b79f5c2c2a7c45c410ef6610ab988
-                }
-          }
-        // for(var i=0;i<data.length;i++){
-        //     for(var j=0;j<data[i].length;j++){
-        //         if(data[i][6]!='xmid')
-        //         setstate([...state,
-        //         {
-        //             x:data[i][6],
-        //             y:data[i][7]
-        //         }]);
-        //     }
-        // }
-
-<<<<<<< HEAD
-
-          console.log(counter)
-    //    console.log(state,'in function')
-=======
->>>>>>> c3574a1f427b79f5c2c2a7c45c410ef6610ab988
     }
     return (
         <div>
@@ -73,16 +50,11 @@ export default function ReadCSV() {
              
             //   console.log(state ," state")  
            }
-<<<<<<< HEAD
-             {state && (<DrawRectangle  state={state} counter={counter}/>)} 
-             
-=======
->>>>>>> c3574a1f427b79f5c2c2a7c45c410ef6610ab988
 
-             {state && state.map((e)=>(<DrawRectangle  state={e}/>))} 
+             {/* {state && state.map((e)=>(<DrawRectangle  state={e}/>))}  */}
              {/* {value && value.map((e,i)=>(<DrawRectangle pOne={e[i]} pTwo={e[i+1]} />))} */}
-         {/* {state && <DrawRectangle state={state} /> } */}
-             <img src={img} /> 
+         {flag && <DrawRectangle state={state} /> }
+             {/* <img src={img} />  */}
                 
            
         </div>
